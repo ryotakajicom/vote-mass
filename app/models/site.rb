3e518f6d09cc2643196227e4a0a45c2f7130c6f1
Ryota Kaji <ryota.kaji531@gmail.com>
2017-04-24 07:15:50 +0000

    bug fix

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,7 @@
 class Site < ApplicationRecord
   validates :site_name, presence: true, length: { maximum: 255 }
   validates :site_url, presence: true, length: { maximum: 255 }
-  validates :site_image, presence: true, length: { maximum: 255 }
+  validates :site_image, presence: true
   has_many:votes
   mount_uploader :site_image, ImageUploader
 end
